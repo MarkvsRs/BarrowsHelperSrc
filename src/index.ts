94b5c3aca93ad041dcaa7ce8509909fa84b3b1e4
Markvs <mark.sild@hotmail.co.uk>
2022-02-21 16:18:41 +0000

    fixed patch

--- src/index.ts
+++ src/index.ts
@@ -406,7 +406,7 @@ function atbarrows(img: ImgRef){
 	
 		if (loc.length == 0){
 			//change status in alt1 browser to "Not at barrows"
-			document.getElementById('Status').textContent =  "Due to update to barrows brothers killed interface on 21st feb 2022 the helper isnt currently working, I'm working on a fix but it may take a coulpe days due to IRL commitments.";//"Not at barrows";
+			document.getElementById('Status').textContent =  "Not at barrows";
 				//reset brother list.
 				var newObject = JSON.stringify(fullbrotherList);
 				brotherList = JSON.parse(newObject);
